0846b0deacfbd6b8117343b6a8251e996fa6d536
POSTA\bojan.m.popovic <bojan.m.popovic@posta.rs>
2017-08-01 15:26:11 +0200

    AutoMapper

---
+++ Vidly/App_Start/mappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class mappingProfile : Profile
+    {
+        public mappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
