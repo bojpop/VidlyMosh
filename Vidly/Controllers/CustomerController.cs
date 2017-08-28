9a612adf0da69757cb8bd652ad5133b3d1cb639f
bojpop <poppfc@gmail.com>
2017-08-29 00:14:45 +0200

    jquery2

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -67,9 +67,9 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            return View(/*customers*/);
         }
 
         // GET: Single Customer
