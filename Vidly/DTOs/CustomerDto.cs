b2d407935705792ee5b8cfef808c8e6735ef5d9d
bojpop <poppfc@gmail.com>
2017-08-30 01:15:57 +0200

    workingJQueryCustomer

--- Vidly/DTOs/CustomerDto.cs
+++ Vidly/DTOs/CustomerDto.cs
@@ -21,5 +21,7 @@ namespace Vidly.DTOs
         public bool IsSubscribedToNewsletter { get; set; }
 
         public byte MembershipTypeId { get; set; }
+        public MembershipTypeDto MembershipType { get; set; }
+
     }
 }
